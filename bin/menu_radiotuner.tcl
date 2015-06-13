9b6a9dc65375b4575646a720de623a152f7c120e
Colin Kong <hob4bit@googlemail.com>
2015-06-13 15:03:12 +0100

    Fix RTHK vlc radio

--- bin/menu_radiotuner.tcl
+++ bin/menu_radiotuner.tcl
@@ -14,8 +14,20 @@ frame .mainmenu -bg "#cccccc"
 # Chinese
     button .mainmenu.988 -width 10 -bg "#ffff00" -text "988最好听" -command {exec vlc --http-reconnect http://starrfm.rastream.com/starrfm-988 &}
     pack .mainmenu.988 -side top
-    button .mainmenu.rthk -width 10 -bg "#ffff00" -text "RTHK" -command {exec firefox http://programme.rthk.hk/channel/radio/player_popup.php?rid=168&player=mp3&type=live &}
+    button .mainmenu.rthk -width 10 -bg "#ffff00" -text "RTHK Radios" -command {exec firefox http://programme.rthk.hk/channel/radio/player_popup.php?rid=168&player=mp3&type=live &}
     pack .mainmenu.rthk -side top
+    button .mainmenu.rthk1 -width 10 -bg "#ffff00" -text "RTHK 1" -command {exec vlc --http-reconnect http://rthk.hk/live1.m3u &}
+    pack .mainmenu.rthk1 -side top
+    button .mainmenu.rthk2 -width 10 -bg "#ffff00" -text "RTHK 2" -command {exec vlc --http-reconnect http://rthk.hk/live2.m3u &}
+    pack .mainmenu.rthk2 -side top
+    button .mainmenu.rthk3 -width 10 -bg "#ffff00" -text "RTHK 3" -command {exec vlc --http-reconnect http://rthk.hk/live3.m3u &}
+    pack .mainmenu.rthk3 -side top
+    button .mainmenu.rthk4 -width 10 -bg "#ffff00" -text "RTHK 4" -command {exec vlc --http-reconnect http://rthk.hk/live4.m3u &}
+    pack .mainmenu.rthk4 -side top
+    button .mainmenu.rthk5 -width 10 -bg "#ffff00" -text "RTHK 5" -command {exec vlc --http-reconnect http://rthk.hk/live5.m3u &}
+    pack .mainmenu.rthk5 -side top
+    button .mainmenu.rthkpth -width 10 -bg "#ffff00" -text "RTHK普通話" -command {exec vlc --http-reconnect http://rthk.hk/livepth.m3u &}
+    pack .mainmenu.rthkpth -side top
 
     button .mainmenu.close -width 10 -bg "#ff0000" -text Close -command exit
     pack .mainmenu.close -side top
