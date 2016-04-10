20112afd014365a9ead71896933369dc9240bea4
Colin Kong <hob4bit@googlemail.com>
2016-04-10 22:22:21 +0100

    Fixed "dhcptabel.py" to supprot multiple network cards

--- bin/dhcptable.py
+++ bin/dhcptable.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Detect DHCP hosts on 192.168.1.0 subnet
+Detect DHCP hosts on /24 subnets
 """
 
 import os
@@ -27,8 +27,10 @@ class Options(object):
         self._detect()
 
     def _detect(self):
-        myip = '127.0.0.1'
-        self._mymac = '00:00:00:00:00:00'
+        self._networks = []
+        mydev = None
+        myip = None
+        mymac = '00:00:00:00:00:00'
         if os.name == 'posix':
             osname = os.uname()[0]
             if osname == 'Linux':
@@ -38,23 +40,12 @@ class Options(object):
                 task.run()
                 for line in task.get_output(' HWaddr | inet addr[a-z]*:'):
                     if ' HWaddr ' in line:
-                        self._arping.set_args(['-I', line.split()[0]])
-                        self._mymac = line.split()[-1]
+                        mydev = line.split()[0]
+                        mymac = line.split()[-1]
                     else:
                         myip = line.split(':')[1].split()[0]
                         if myip not in ('', '127.0.0.1'):
-                            break
-            elif osname == 'SunOS':
-                ifconfig = command_mod.CommandFile('/sbin/ifconfig', args=['-a'])
-                task = subtask_mod.Batch(ifconfig.get_cmdline())
-                task.run(pattern='\tinet [^ ]+ netmask')
-                for line in task.get_output():
-                    myip = line.split()[1]
-                    if myip not in ('', '127.0.0.1'):
-                        break
-
-        self._myip = myip
-        self._subnet = self._myip.rsplit('.', 1)[0]
+                            self._networks.append((mydev, myip, mymac))
 
     def get_arping(self):
         """
@@ -62,23 +53,11 @@ class Options(object):
         """
         return self._arping
 
-    def get_myip(self):
+    def get_networks(self):
         """
-        Return my IP address.
+        Return networks in (ip, mac) format.
         """
-        return self._myip
-
-    def get_mymac(self):
-        """
-        Return my MAC address.
-        """
-        return self._mymac
-
-    def get_subnet(self):
-        """
-        Return subnet.
-        """
-        return self._subnet
+        return self._networks
 
 
 class ScanHost(threading.Thread):
@@ -86,12 +65,19 @@ class ScanHost(threading.Thread):
     Scan host class
     """
 
-    def __init__(self, options, ip):
-        self._options = options
-        threading.Thread.__init__(self)
+    def __init__(self, arping, network, ip):
+        self._cmdline = arping.get_cmdline() + ['-I', network[0]]
+        self._network = network
         self._ip = ip
+        threading.Thread.__init__(self)
         self._child = None
         self._output = ''
+
+    def get_network(self):
+        """
+        Return network as (mynet, myip, mymac).
+        """
+        return self._network
 
     def get_ip(self):
         """
@@ -109,8 +95,8 @@ class ScanHost(threading.Thread):
         """
         Start thread
         """
-        cmdline = self._options.get_arping().get_cmdline() + ['-c', '3', self._ip]
-        self._child = subtask_mod.Child(cmdline).run(error2output=True)
+        self._child = subtask_mod.Child(
+            self._cmdline + ['-c', '3', self._ip]).run(error2output=True)
         while True:
             byte = self._child.stdout.read(1)
             if not byte:
@@ -149,23 +135,26 @@ class Main(object):
             signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
     def _detect(self):
-        for host in range(1, 255):
-            ip_address = self._options.get_subnet() + '.' + str(host)
-            thread = ScanHost(self._options, ip_address)
-            thread.start()
-            self._threads.append(thread)
+        arping = self._options.get_arping()
+        for network in self._options.get_networks():
+            subnet = network[1].rsplit('.', 1)[0]
+            for ip_address in (subnet + '.' + str(x) for x in range(1, 255)):
+                thread = ScanHost(arping, network, ip_address)
+                thread.start()
+                self._threads.append(thread)
 
     def _output(self):
         for thread in self._threads:
+            network = thread.get_network()
             ip_address = thread.get_ip()
-            if ip_address == self._options.get_myip():
-                print('{0:>11s} [{1:s}]  0.000ms  {2:s}'.format(
-                    ip_address, self._options.get_mymac(), self._reverse_dns(ip_address)))
+            if ip_address == network[1]:
+                print('{0:15s} [{1:s}]   0.000ms  {2:s}'.format(
+                    network[1], network[2], self._reverse_dns(ip_address)))
             else:
                 for line in thread.get_output().split('\n'):
                     if line.startswith('Unicast reply from ' + ip_address):
                         mac, ping = line.split()[4:6]
-                        print('{0:>11s} {1:s}  {2:s}  {3:s}'.format(
+                        print('{0:15s} {1:s} {2:>9s}  {3:s}'.format(
                             ip_address, mac, ping, self._reverse_dns(ip_address)))
                         break
 
