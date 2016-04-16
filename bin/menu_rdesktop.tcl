f66cf31398e60af9f66965a2f8ecf6b2b170c6f0
Colin Kong <hob4bit@googlemail.com>
2016-04-16 08:39:05 +0100

    Changed from "rdesktop" to "xfreerdp"

--- bin/menu_rdesktop.tcl
+++ bin/menu_rdesktop.tcl
@@ -3,26 +3,28 @@
 . config -bg "#cccccc"
 
 
-frame .top -bg "#cccccc"
-    entry .top.hostname -width 13 -relief sunken -textvar hostname
-    pack .top.hostname -side left
-pack .top -fill x
+frame .location -bg "#cccccc"
+    entry .location.hostname -width 26 -relief sunken -textvar hostname
+    pack .location.hostname -side top
+pack .location -fill x
 
-frame .middle -bg "#cccccc"
-    entry .middle.xsize -width 4 -relief sunken -textvar xsize
-    pack .middle.xsize -side left
-    pack .middle -side top -fill x
-    button .middle.reset -width 1 -bg "#ffff00" -text "X" -command { set xsize {1024}; set ysize {768} }
-    pack .middle.reset -side left
-    entry .middle.ysize -width 4 -relief sunken -textvar ysize
-    pack .middle.ysize -side left
-pack .middle -fill x
+frame .login -bg "#cccccc"
+    entry .login.username -width 13 -relief sunken -textvar username
+    pack .login.username -side left
+    entry .login.password -width 13 -relief sunken -textvar password -show "*"
+    pack .login.password -side left
+pack .login -fill x
 
-frame .bottom -bg "#cccccc"
-    button .bottom.connect -width 10 -bg "#00ff00" -text "Connect" -command {exec rdesktop -d "" -u "" -g ${xsize}x$ysize -a 24 ${hostname} &}
-    pack .bottom.connect -side top
-pack .bottom -side top -fill x
+frame .connect -bg "#cccccc"
+    entry .connect.size -width 13 -relief sunken -textvar size
+    pack .connect.size -side left
+    button .connect.run -width 10 -bg "#00ff00" -text "Connect" -command {
+        exec xfreerdp /cert-ignore /v:${hostname} /size:${size} /u:${username} /p:${password} &
+    }
+    pack .connect.run -side left
+pack .connect -fill x
 
 set hostname {hostname.local}
-set xsize {1024}
-set ysize {768}
+set username {domain\user}
+set password {???}
+set size {1024x768}
