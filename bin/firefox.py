e35b9c07ea646d3e5d0cc617501fc5186fcef918
owner <owner@webtv>
2016-04-18 20:38:22 +0100

    Disable safe browsing spying in "firefox.py"

--- bin/firefox.py
+++ bin/firefox.py
@@ -226,6 +226,8 @@ class Options(object):
                             for setting in ('"accessibility.typeaheadfind.enablesound", false',
                                             '"browser.blink_allowed", false',
                                             '"browser.bookmarks.max_backups", 1',
+                                            '"browser.safebrowsing.enabled", false',
+                                            '"browser.safebrowsing.malware.enabled", false',
                                             '"browser.cache.memory.capacity", 16384',
                                             '"browser.display.show_image_placeholders", false',
                                             '"browser.download.animateNotifications", false',
