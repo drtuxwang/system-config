e787940ed1aecf366c3ee07206d963d401a29164
Colin Kong <hob4bit@googlemail.com>
2016-04-24 11:04:15 +0100

    Added "unsql.py" for viewing Sqlite database files

---
+++ bin/unsql.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+"""
+Unpack a sqlite database file.
+"""
+
+import argparse
+import glob
+import os
+import signal
+import sqlite3
+import sys
+
+if sys.version_info < (3, 2) or sys.version_info >= (4, 0):
+    sys.exit(__file__ + ': Requires Python version (>= 3.2, < 4.0).')
+
+
+class Options(object):
+    """
+    Options class
+    """
+
+    def __init__(self):
+        self._args = None
+        self.parse(sys.argv)
+
+    def get_files(self):
+        """
+        Return list of files.
+        """
+        return self._args.files
+
+    def _parse_args(self, args):
+        parser = argparse.ArgumentParser(description='Unpack a sqlite database file.')
+
+        parser.add_argument('files', nargs='+', metavar='file.sqlite', help='Sqlite database file.')
+
+        self._args = parser.parse_args(args)
+
+    def parse(self, args):
+        """
+        Parse arguments
+        """
+        self._parse_args(args[1:])
+
+
+class Main(object):
+    """
+    Main class
+    """
+
+    def __init__(self):
+        try:
+            self.config()
+            sys.exit(self.run())
+        except (EOFError, KeyboardInterrupt):
+            sys.exit(114)
+        except SystemExit as exception:
+            sys.exit(exception)
+
+    @staticmethod
+    def config():
+        """
+        Configure program
+        """
+        if hasattr(signal, 'SIGPIPE'):
+            signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+        if os.name == 'nt':
+            argv = []
+            for arg in sys.argv:
+                files = glob.glob(arg)  # Fixes Windows globbing bug
+                if files:
+                    argv.extend(files)
+                else:
+                    argv.append(arg)
+            sys.argv = argv
+
+    @staticmethod
+    def _show(file):
+        with sqlite3.connect(file) as conn:
+            print(file + ':')
+            try:
+                for line in conn.iterdump():
+                    print('   ', line)
+            except sqlite3.DatabaseError as exception:
+                raise SystemExit(sys.argv[0] + ': ' + str(exception))
+
+    @classmethod
+    def run(cls):
+        """
+        Start program
+        """
+        options = Options()
+
+        for file in options.get_files():
+            if not os.path.isfile(file):
+                raise SystemExit(sys.argv[0] + ': Cannot find "' + file + '" database file.')
+            cls._show(file)
+
+
+if __name__ == '__main__':
+    if '--pydoc' in sys.argv:
+        help(__name__)
+    else:
+        Main()
