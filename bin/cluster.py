d4ee8b7bbc5f51cbde206546cc7f4817007c89d8
Colin Kong <hob4bit@googlemail.com>
2016-05-22 09:29:19 +0100

    Complete re-write of "cluster.py" usign Paramiko

--- bin/cluster.py
+++ bin/cluster.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 """
-Run command on a subnet in parallel.
+Run command on a list of nodes in parallel.
 """
 
 import argparse
 import glob
 import os
-import re
+import queue
 import signal
+import subprocess
 import sys
-import threading
 import time
+import threading
 
-import command_mod
-import subtask_mod
+import paramiko
 
-if sys.version_info < (3, 2) or sys.version_info >= (4, 0):
-    sys.exit(sys.argv[0] + ': Requires Python version (>= 3.2, < 4.0).')
+if sys.version_info < (3, 3) or sys.version_info >= (4, 0):
+    sys.exit(sys.argv[0] + ': Requires Python version (>= 3.3, < 4.0).')
 
 
 class Options(object):
@@ -28,132 +28,186 @@ class Options(object):
         self._args = None
         self.parse(sys.argv)
 
-    def get_command_line(self):
+    def get_threads(self):
         """
-        Return command line.
+        Return number of threads.
         """
-        return self._args.command + self._command_args
+        return self._args.threads[0]
 
-    def get_ssh(self):
+    def get_timeout(self):
         """
-        Return ssh Command class object.
+        Return timeout in seconds.
         """
-        return self._ssh
+        return self._args.timeout[0]
 
-    def get_subnet(self):
+    def get_nodes(self):
         """
-        Return subnet.
+        Return nodes ist.
         """
-        return self._subnet
+        return self._nodes
+
+    def get_cmdline(self):
+        """
+        Return the command line as a list.
+        """
+        return self._cmdline
 
     @staticmethod
-    def _getmyip():
-        myip = ''
-        if os.name == 'posix':
-            osname = os.uname()[0]
-            if osname == 'Linux':
-                os.environ['LANG'] = 'en_GB'
-                ifconfig = command_mod.CommandFile('/sbin/ifconfig', args=['-a'])
-                task = subtask_mod.Batch(ifconfig.get_cmdline())
-                task.run(pattern=' inet addr[a-z]*:')
-                for line in task.get_output():
-                    myip = line.split(':')[1].split()[0]
-                    if myip not in ('', '127.0.0.1'):
-                        break
-            elif osname == 'SunOS':
-                ifconfig = command_mod.CommandFile('/sbin/ifconfig', args=['-a'])
-                task = subtask_mod.Batch(ifconfig.get_cmdline())
-                task.run(pattern='\tinet [^ ]+ netmask')
-                for line in task.get_output():
-                    myip = line.split()[1]
-                    if myip not in ('', '127.0.0.1'):
-                        break
-        return myip
+    def _parse_input():
+        nodes = []
+        for line in sys.stdin:
+            nodes.extend(line.split())
+        return nodes
 
     def _parse_args(self, args):
-        parser = argparse.ArgumentParser(description='Run command on a subnet in parallel.\n')
+        parser = argparse.ArgumentParser(
+            description='Run command on a list of nodes in parallel.\n')
 
-        parser.add_argument('-subnet', nargs=1, metavar='netmask',
-                            help='Select netmask in the format "192.168.149.0".')
+        parser.add_argument('-threads', nargs=1, type=int, dest='threads', default=[8],
+                            metavar='N', help='Select number of threads. Default is 8.')
+        parser.add_argument('-timeout', nargs=1, type=int, dest='timeout', default=[10],
+                            metavar='seconds', help='Select timeout in seconds. Default is 10.')
 
         parser.add_argument('command', nargs=1, help='Command to run on all systems.')
-        parser.add_argument('args', nargs='*', metavar='arg', help='Command argument.')
+        parser.add_argument('args', nargs='*', metavar='arg', help='Command arguments.')
 
         my_args = []
         while len(args):
             my_args.append(args[0])
             if not args[0].startswith('-'):
                 break
-            elif args[0] == '-subnet' and len(args) >= 2:
+            elif args[0] == '-threads' and len(args) >= 2:
+                args = args[1:]
+                my_args.append(args[0])
+            elif args[0] == '-timeout' and len(args) >= 2:
                 args = args[1:]
                 my_args.append(args[0])
             args = args[1:]
 
         self._args = parser.parse_args(my_args)
+        return args
 
     def parse(self, args):
         """
         Parse arguments
         """
-        self._parse_args(args[1:])
-
-        self._command_args = args[1:]
-
-        issubnet = re.compile(r'^\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]0$')
-        if self._args.subnet and issubnet.match(self._args.subnet[0]):
-            self._subnet = self._args.subnet[0]
-        else:
-            myip = self._getmyip()
-            if not myip:
-                raise SystemExit(sys.argv[0] + ": Cannot determine subnet configuration.")
-            self._subnet = myip.rsplit('.', 1)[0]
-
-        self._ssh = command_mod.Command('ssh', errors='stop')
-        self._ssh.set_args(['-o', 'StrictHostKeyChecking=no', '-o',
-                            'UserKnownHostsFile=/dev/null', '-o', 'BatchMode=yes'])
+        self._cmdline = self._parse_args(args[1:])
+        self._nodes = self._parse_input()
 
 
-class Remote(threading.Thread):
+class SecureShell(object):
     """
-    Remote class
+    SecureShell class
     """
 
-    def __init__(self, options, ip):
-        self._options = options
-        threading.Thread.__init__(self)
-        self._ip = ip
-        self._output = ''
-        self._child = None
+    def __init__(self):
+        self._client = paramiko.SSHClient()
+        self._client.load_system_host_keys()
+        self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self._host = None
 
-    def get_output(self):
-        """
-        Get output
-        """
-        return self._output
+    def __del__(self):
+        self._client.close()
 
-    def run(self):
+    def connect(self, host):
         """
-        Run command
+        Connect to host
         """
-        ssh = self._options.get_ssh()
-        ssh.set_args([self._ip, 'echo "=== ' + self._ip + ': "`uname -s -n`" ==="; ' + ssh.args2cmd(
-            self._options.get_command_line())])
+        self._host = host
 
-        self._child = subtask_mod.Child(ssh.get_cmdline()).run(error2output=True)
-        self._child.stdin.close()
+        try:
+            self._client.connect(self._host, look_for_keys=False, timeout=4)
+        except Exception as exception:
+            raise SecureShellError(exception)
+
+    def execute(self, command, timeout):
+        """
+        Execute command on remote node
+
+        command = Command to run
+        timeout = Output timeout inseconds
+        """
+
+        _, stdout, _ = self._client.exec_command(command, get_pty=True, timeout=timeout)
+        try:
+            with open(self._host + '.log', 'w', newline='\n') as ofile:
+                for line in stdout:
+                    print(line.rstrip('\r\n'), file=ofile)
+        except Exception as exception:
+            raise SecureShellError(exception)
+
+
+class WorkQueue(object):
+    """
+    WorkQueue class
+    """
+
+    def __init__(self, threads, command, timeout):
+        """
+        threads = Number of threads
+        data    = Work data
+        """
+        self._time0 = time.time()
+        self._nitems = 0
+        self._queue = queue.Queue(maxsize=0)
+
+        self._workers = []
+        for _ in range(threads):
+            worker = threading.Thread(target=self._do_work, args=(command, timeout))
+            worker.setDaemon(True)
+            worker.start()
+            self._workers.append(worker)
+
+    def _show_start(self, host, message):
+        print('[{0:d},{1:d}] {2:s}: {3:s}'.format(
+            int((self._nitems - self._queue.qsize()) / self._nitems * 100),
+            int(time.time() - self._time0), host, message))
+
+    def _show_finish(self, host, message):
+        print('[{0:d},{1:d}] {2:s}: {3:s}'.format(
+            int((self._nitems - self._queue.qsize() + 1) / self._nitems * 100),
+            int(time.time() - self._time0), host, message))
+
+    def _do_work(self, command, timeout):
         while True:
-            byte = self._child.stdout.read(1)
-            if not byte:
+            host = self._queue.get()
+            if host is None:
                 break
-            self._output += byte.decode('utf-8', 'replace')
+            ssh = SecureShell()
 
-    def kill(self):
+            try:
+                ssh.connect(host)
+                self._show_start(host, 'connected')
+                ssh.execute(subprocess.list2cmdline(command), timeout)
+            except SecureShellError as exception:
+                self._show_finish(host, str(exception))
+            else:
+                self._show_finish(host, 'completed')
+            self._queue.task_done()
+
+    def add_items(self, hosts):
         """
-        Kill thread
+        Add items to queue
         """
-        if self._child:
-            self._child.kill()
-            self._child = None
+        self._nitems += len(hosts)
+        for host in hosts:
+            self._queue.put(host)
+
+    def join(self):
+        """
+        Close down queue
+        """
+        for worker in self._workers:
+            self._queue.put(None)
+            self._nitems += 1
+        for worker in self._workers:
+            worker.join()
+
+
+class SecureShellError(Exception):
+    """
+    Secure shell class error.
+    """
 
 
 class Main(object):
@@ -187,65 +241,17 @@ class Main(object):
                     argv.append(arg)
             sys.argv = argv
 
-    def _allreduce(self):
-        print('\rAllreduce from subnet "' + self._options.get_subnet() + '.0"...')
-        obytes = 0
-        same = 0
-        alive = True
-
-        while alive:
-            nbytes = 0
-            alive = False
-            for thread in self._threads:
-                nbytes += len(thread.get_output())
-                if thread.is_alive():
-                    alive = True
-            sys.stdout.write('\r  -> Received ' + str(nbytes) + ' bytes...')
-            sys.stdout.flush()
-            if nbytes == obytes:
-                same += 1
-                if same > self._wait_max:
-                    break
-            else:
-                same = 0
-                obytes = nbytes
-            time.sleep(self._wait_time)
-        print()
-
-    def _bcast(self):
-        print('Bcast to subnet "' + self._options.get_subnet() + '.0"...')
-        for host in range(1, 255):
-            ip_address = self._options.get_subnet() + '.' + str(host)
-            sys.stdout.write('\r  -> ' + str(ip_address) + '...')
-            sys.stdout.flush()
-            thread = Remote(self._options, ip_address)
-            thread.start()
-            self._threads.append(thread)
-
-    def _output(self):
-        iserror = re.compile('Command not supported|Network is unreachable|No route to host|'
-                             'Permission denied.|protocol failure in circuit setup', re.IGNORECASE)
-
-        for thread in self._threads:
-            output = thread.get_output()
-            if output and not iserror.search(output):
-                print(thread.get_output().rstrip('\r\n'))
-
-        for thread in self._threads:
-            thread.kill()
-
     def run(self):
         """
         Start program
         """
         self._options = Options()
-        self._wait_max = 64
-        self._wait_time = 0.1
 
-        self._threads = []
-        self._bcast()
-        self._allreduce()
-        self._output()
+        nodes = self._options.get_nodes()
+        threads = min(len(nodes), self._options.get_threads())
+        work_queue = WorkQueue(threads, self._options.get_cmdline(), self._options.get_timeout())
+        work_queue.add_items(nodes)
+        work_queue.join()
 
 
 if __name__ == '__main__':
