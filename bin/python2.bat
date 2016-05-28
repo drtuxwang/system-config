7a42dbea1a1ba953638b6ef2a62c4322335242c4
Colin Kong <hob4bit@googlemail.com>
2016-05-28 13:36:34 +0100

    Updated to Python 2.7.11 & 3.4.4 on Windows

--- bin/python2.bat
+++ bin/python2.bat
@@ -1,8 +1,8 @@
 @echo off
 if not "%PROCESSOR_ARCHITECTURE%"=="AMD64" goto x86
-if not exist %~dp0..\python_2.7.9\windows64_5.1-x86\python.exe goto x86
-    %~dp0..\python_2.7.9\windows64_5.1-x86\python.exe -B %*
+if not exist %~dp0..\python_2.7.11\windows_5.1-x86\python.exe goto x86
+    %~dp0..\python_2.7.11\windows_5.1-x86\python.exe -B %*
     goto end
 :x86
-    %~dp0..\python_2.7.9\windows_5.1-x86\python.exe -B %*
+    %~dp0..\python_2.7.11\windows_5.1-x86\python.exe -B %*
 :end
