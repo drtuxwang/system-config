4d8e3951f93c6e3b2549a5102dcd7fcb537510a7
Colin Kong <hob4bit@googlemail.com>
2016-06-03 15:41:08 +0100

    Updated "autoexec-local.sh" for VirtualBox detection

--- config/autoexec-local.sh
+++ config/autoexec-local.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-# xrandr --newmode "1280x960" 102.10 1280 1360 1496 1712 960 961 964 994 # gtf 1280 960 60
-# xrandr --addmode VGA-0 1280x960
-# (sleep 1; xrandr --dpi 96; sleep 1; xrandr -s 1024x768) &
+if [ "`lspci 2> /dev/null | grep VirtualBox`" ]; then
+    # xrandr --newmode "1280x960" 102.10 1280 1360 1496 1712 960 961 964 994 # gtf 1280 960 60
+    # xrandr --addmode VGA-0 1280x960
+    (sleep 1; xrandr --dpi 96; sleep 1; xrandr -s 1024x768) &
+fi
 
 # setxkbmap us  # "gb", "de", "us"
 # xset m 2,16  # Slow mouse
