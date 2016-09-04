318a1a9911459079d3588cf114941c4857e62f20
Colin Kong <hob4bit@googlemail.com>
2016-09-04 06:37:59 +0100

    Workaround "Resource temporarily unavailable" during "chrome" startup

--- config/autoexec-local.sh
+++ config/autoexec-local.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+start_app()
+{
+    for TRY in `seq 10`; do
+    if [ ! "`ps | grep \" $1\$\"`" ]; then
+       echo "Starting \"$1\"..."
+       $1 &
+    fi
+    sleep 1
+    done
+}
+
+
 if [ "`lspci 2> /dev/null | grep VirtualBox`" ]; then
     # xrandr --newmode "1280x960" 102.10 1280 1360 1496 1712 960 961 964 994 # gtf 1280 960 60
     # xrandr --addmode VGA-0 1280x960
@@ -10,7 +22,6 @@ fi
 # xset m 2,16  # Slow mouse
 
 # xreset
-# (sleep 1; firefox)
-# (sleep 1; chrome)
-
+# start_app chrome &
+# start_app firefox &
 # myqsd 1
