ac2a7ced3c215299a1949058eb5fb52b63776685
Colin Kong <hob4bit@googlemail.com>
2016-09-25 14:09:50 +0100

    Add "set_vga()" for fixing VGA modeline

--- config/autoexec-local.sh
+++ config/autoexec-local.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
+set_vga()
+{   
+    MODELINE=$(gtf 1440 900 60 | grep Modeline | awk '{
+        printf("%4.2f %d %d %d %d %d %d %d %d\n", $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+    }')
+    echo xrandr --newmode $2x${3}_$4 $MODELINE
+    echo xrandr --addmode $1 $2x${3}_$4
+    echo xrandr -s $2x$3
+}
+
 start_app()
 {
     echo "Starting \"$1\"..."
     "$@" &
     sleep 4
-    for _ in `seq 10 `; do
+    for _ in `seq 11`; do
         sleep 1
         if [ ! "`ps | grep \" $1\$\"`" ]; then
             echo "Restarting \"$1\"..."
@@ -17,15 +27,13 @@ start_app()
 
 
 if [ "`lspci 2> /dev/null | grep VirtualBox`" ]; then
-    # xrandr --newmode "1280x960" 102.10 1280 1360 1496 1712 960 961 964 994 # gtf 1280 960 60
-    # xrandr --addmode VGA-0 1280x960
     (sleep 1; xrandr --dpi 96; sleep 1; xrandr -s 1024x768) &
 fi
-
+# set_vga VGA1 gtf 1440 900 60
+# xreset
 # setxkbmap us  # "gb", "de", "us"
 # xset m 2,16  # Slow mouse
 
-# xreset
 # start_app chrome &
 # start_app firefox &
 # myqsd 1
