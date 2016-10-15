0f641710128b7af1a607c8e4568dcc1c0c3b9a1f
Colin Kong <hob4bit@googlemail.com>
2016-10-15 16:48:48 +0100

    Update "chrome.py -reset" to remove https* storage

--- bin/chrome.py
+++ bin/chrome.py
@@ -239,6 +239,8 @@ class Options(object):
                 ):
                     print('Removing "{0:s}"...'.format(directory))
                     self._remove(directory)
+                for file in glob.glob(os.path.join(directory, 'Local Storage', 'https*')):
+                    self._remove(file)
 
     @classmethod
     def _restart(cls):
