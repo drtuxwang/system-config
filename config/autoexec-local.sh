273660be047425a95a58d6dbc04866c873895cf2
Colin Kong <hob4bit@googlemail.com>
2017-01-07 18:01:44 +0000

    Updated "autoexec-local.sh" with DVI-0 resolution fix example

--- config/autoexec-local.sh
+++ config/autoexec-local.sh
@@ -1,9 +1,9 @@
 # set_vga VGA-0 1024 768 60 &
 # set_vga VGA1 1440 900 60 &
-# xreset
+# xreset DVI-I=1360x768
 # setxkbmap us  # "gb", "de", "us"
 # xset m 2,16  # Slow mouse
 
-# start_app -timeout=60 chrome &
+# start_app -timeout=30 chrome &
 # start_app firefox &
 # myqsd 1
