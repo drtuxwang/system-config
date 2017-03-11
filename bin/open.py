0169a3f8a479c75d422dd619034bda9b8077558f
Dr Colin Kong <hob4bit@googlemail.com>
2017-03-11 18:34:17 +0000

    Change "open.py" to use "chrome" for Chromium instead of Firefox

--- bin/open.py
+++ bin/open.py
@@ -15,7 +15,7 @@ import subtask_mod
 if sys.version_info < (3, 2) or sys.version_info >= (4, 0):
     sys.exit(__file__ + ': Requires Python version (>= 3.2, < 4.0).')
 
-BROWSER = 'firefox'
+BROWSER = 'chrome'
 MAPPINGS = {
     'htm': BROWSER,
     'html': BROWSER,
