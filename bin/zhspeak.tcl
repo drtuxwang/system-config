f86b1a6533719ac14201dc28c151bd1bea9e9a5e
Dr Colin Kong <hob4bit@googlemail.com>
2017-03-12 07:16:45 +0000

    Update "zhspeak.tcl" coding style

--- bin/zhspeak.tcl
+++ bin/zhspeak.tcl
@@ -14,33 +14,57 @@ pack .input -side top -fill x
 frame .toolbar -bg "#cccccc"
 
     pack .toolbar -side top -fill x
-    button .toolbar.new -width 4 -bg "#ff0000" -text "New" -command { set text {}; set msg {} }
+    button .toolbar.new -width 4 -bg "#ff0000" -text "New" -command {set text {}; set msg {}}
     pack .toolbar.new -side left
-    button .toolbar.copy -width 4 -bg "#ffcc00" -text "Copy" -command { exec echo "echo $text | xclip -in" | /bin/sh & }
+    button .toolbar.copy -width 4 -bg "#ffcc00" -text "Copy" -command {
+        exec echo "echo $text | xclip -in" | /bin/sh &
+    }
     pack .toolbar.copy -side left
-    button .toolbar.paste -width 4 -bg "#ffcc00" -text "Paste" -command { catch { exec xclip -out -selection -c text } text; set msg {} }
+    button .toolbar.paste -width 4 -bg "#ffcc00" -text "Paste" -command {
+        catch {exec xclip -out -selection -c text} text; set msg {}
+    }
     pack .toolbar.paste -side left
 
-    button .toolbar.pinyin -width 3 -bg "#00ff00" -text py -command { catch { exec zhspeak -pinyin -zh "$text" } msg }
+    button .toolbar.pinyin -width 3 -bg "#00ff00" -text py -command {
+        catch {exec zhspeak -pinyin -zh "$text"} msg
+    }
     pack .toolbar.pinyin -side right
-    button .toolbar.zhy -width 3 -bg "#00ff00" -text zhy -command { catch { exec zhspeak -zhy "$text" } msg }
+    button .toolbar.zhy -width 3 -bg "#00ff00" -text zhy -command {
+        catch {exec zhspeak -zhy "$text"} msg
+    }
     pack .toolbar.zhy -side right
-    button .toolbar.zh -width 3 -bg "#00ff00" -text zh -command { catch { exec zhspeak -zh "$text" } msg }
+    button .toolbar.zh -width 3 -bg "#00ff00" -text zh -command {
+        catch {exec zhspeak -zh "$text"} msg
+    }
     pack .toolbar.zh -side right
-    button .toolbar.en -width 3 -bg "#00ff00" -text en -command { catch { exec zhspeak -en "$text" } msg }
+    button .toolbar.en -width 3 -bg "#00ff00" -text en -command {
+        catch {exec zhspeak -en "$text"} msg
+    }
     pack .toolbar.en -side right
 
-    button .toolbar.sr -width 3 -bg "#ffff00" -text sr -command { catch { exec zhspeak -sr "$text" } msg }
+    button .toolbar.sr -width 3 -bg "#ffff00" -text sr -command {
+       catch {exec zhspeak -sr "$text"} msg
+    }
     pack .toolbar.sr -side right
-    button .toolbar.ru -width 3 -bg "#ffff00" -text ru -command { catch { exec zhspeak -ru "$text" } msg }
+    button .toolbar.ru -width 3 -bg "#ffff00" -text ru -command {
+        catch {exec zhspeak -ru "$text"} msg
+    }
     pack .toolbar.ru -side right
-    button .toolbar.it -width 3 -bg "#ffff00" -text it -command { catch { exec zhspeak -it "$text" } msg }
+    button .toolbar.it -width 3 -bg "#ffff00" -text it -command {
+       catch {exec zhspeak -it "$text"} msg
+    }
     pack .toolbar.it -side right
-    button .toolbar.fr -width 3 -bg "#ffff00" -text fr -command { catch { exec zhspeak -fr "$text" } msg }
+    button .toolbar.fr -width 3 -bg "#ffff00" -text fr -command {
+        catch {exec zhspeak -fr "$text"} msg
+    }
     pack .toolbar.fr -side right
-    button .toolbar.es -width 3 -bg "#ffff00" -text es -command { catch { exec zhspeak -es "$text" } msg }
+    button .toolbar.es -width 3 -bg "#ffff00" -text es -command {
+        catch {exec zhspeak -es "$text"} msg
+    }
     pack .toolbar.es -side right
-    button .toolbar.de -width 3 -bg "#ffff00" -text de -command { catch { exec zhspeak -de "$text" } msg }
+    button .toolbar.de -width 3 -bg "#ffff00" -text de -command {
+        catch {exec zhspeak -de "$text"} msg
+    }
     pack .toolbar.de -side right
 
 pack .toolbar -side top -fill x
