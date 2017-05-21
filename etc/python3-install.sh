8fa4654b15cb26aab69939d0d4f8f9485175a2e0
Dr Colin Kong <hob4bit@googlemail.com>
2017-05-21 07:08:49 +0100

    Add "python3-install.sh" installer

---
+++ etc/python3-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Check pip modules and install minimum version required
+#
+
+if [ -w "$(python3 -help 2>&1 | grep usage: | awk '{print $2}')" ]
+then
+    INSTALL="python3 -m pip install"
+else
+    INSTALL="python3 -m pip install --user"
+fi
+
+LIST=$(python3 -m pip list)
+for PIP in $(cat ${0%/*}/python3-requirements.txt)
+do
+    MODULE=${PIP%=*}
+    if [ ! "$(echo "$LIST" | grep "^MODULE ")" ]
+    then
+        $INSTALL ${PIP/>=/==}
+    fi
+done
+
+$INSTALL -r ${0%/*}/python3-requirements.txt
