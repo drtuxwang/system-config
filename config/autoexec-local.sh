923c7c75f86f34c8fad59f03240c1cc9b85f6100
Colin Kong <hob4bit@googlemail.com>
2017-08-27 12:41:56 +0100

    Add comments to HDMI port auto selection examples

--- config/autoexec-local.sh
+++ config/autoexec-local.sh
@@ -1,8 +1,8 @@
 # set_vga VGA-0 1024 768 60 &
 # set_vga VGA1 1440 900 60 &
-# pactl set-card-profile 0 output:hdmi-stereo+input:analog-stereo
-# pactl set-card-profile 0 output:hdmi-stereo-extra1+input:analog-stereo
 # xreset DVI-I=1360x768
+# pactl set-card-profile 0 output:hdmi-stereo+input:analog-stereo  # HDMI-1
+# pactl set-card-profile 0 output:hdmi-stereo-extra1+input:analog-stereo  # HDMI-2
 # setxkbmap us  # "gb", "de", "us"
 # xset m 2,16  # Slow mouse
 
