06c343c3be67ae16dad658d325e164d108fbee3a
Dr Colin Kong <hob4bit@googlemail.com>
2018-01-14 06:22:47 +0000

    Revert "Change "fsame.py" to show full path"
    This reverts commit ac263c7e50e18574cc902a8d39f44b7176326dc0.

--- bin/fsame.py
+++ bin/fsame.py
@@ -166,10 +166,7 @@ class Main(object):
 
         for files in sorted(self._md5files.values()):
             if len(files) > 1:
-                sorted_files = [
-                    os.path.abspath(file)
-                    for file in sorted(files)
-                ]
+                sorted_files = sorted(files)
                 print('***', command_mod.Command.args2cmd(sorted_files), '***')
                 if options.get_remove_flag():
                     self._remove(sorted_files[1:])
