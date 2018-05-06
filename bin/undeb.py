0debc4b48a93b35ebe0e726931234b8468e169a4
owner <hob4bit@googlemail.com>
2018-05-06 16:57:52 +0100

    Fix "undeb.py" for "control.tar.xz" files

--- bin/undeb.py
+++ bin/undeb.py
@@ -103,9 +103,9 @@ class Main(object):
                 pass
 
     def _unpack(self, file):
-        task = subtask_mod.Batch(self._ar.get_cmdline() + ['tv', file])
-        task.run(pattern='data.tar')
-        if len(task.get_output()) != 1:
+        task = subtask_mod.Batch(self._ar.get_cmdline() + ['t', file])
+        task.run(pattern='(data|control).tar')
+        if len(task.get_output()) != 2:
             raise SystemExit(
                 sys.argv[0] + ': Cannot read "' + file + '" DEB file.')
         elif task.get_exitcode():
@@ -113,63 +113,45 @@ class Main(object):
                 sys.argv[0] + ': Error code ' + str(task.get_exitcode()) +
                 ' received from "' + task.get_file() + '".'
             )
-        archive = task.get_output()[-1].split()[-1]
-        self._remove(archive, 'data.tar')
-
-        task2 = subtask_mod.Task(self._ar.get_cmdline() + ['x', file, archive])
-        task2.run()
-        if task2.get_exitcode():
-            raise SystemExit(
-                sys.argv[0] + ': Error code ' + str(task2.get_exitcode()) +
-                ' received from "' + task2.get_file() + '".'
-            )
-
-        task = subtask_mod.Batch(self._p7zip.get_cmdline() + [archive])
-        task.run()
-        if task.get_exitcode():
-            raise SystemExit(
-                sys.argv[0] + ': Error code ' + str(task.get_exitcode()) +
-                ' received from "' + task.get_file() + '".'
-            )
-
-        task2 = subtask_mod.Task(self._tar.get_cmdline() + ['data.tar'])
-        task2.run()
-        if task2.get_exitcode():
-            raise SystemExit(
-                sys.argv[0] + ': Error code ' + str(task2.get_exitcode()) +
-                ' received from "' + task2.get_file() + '".'
-            )
-        self._remove(archive, 'data.tar')
+        data_file = task.get_output('data.tar.*')[-1]
+        control_file = task.get_output('control.tar.*')[-1]
 
         task = subtask_mod.Batch(
-            self._ar.get_cmdline() + ['x', file, 'control.tar.gz'])
+            self._ar.get_cmdline() + ['x', file, data_file]
+        )
         task.run()
         if task.get_exitcode():
             raise SystemExit(
                 sys.argv[0] + ': Error code ' + str(task.get_exitcode()) +
                 ' received from "' + task.get_file() + '".'
             )
-        if not self._options.get_view_flag() and not os.path.isdir('DEBIAN'):
+        task2 = subtask_mod.Task(self._tar.get_cmdline() + [data_file])
+        task2.run()
+        self._remove(data_file)
+        if task2.get_exitcode():
+            raise SystemExit(
+                sys.argv[0] + ': Error code ' + str(task2.get_exitcode()) +
+                ' received from "' + task2.get_file() + '".'
+            )
+
+        task = subtask_mod.Batch(
+            self._ar.get_cmdline() + ['x', file, control_file]
+        )
+        task.run()
+        if self._options.get_view_flag():
+            task2 = subtask_mod.Task(self._tar.get_cmdline() + [control_file])
+            task2.run(replace=(os.curdir, 'DEBIAN'))
+        else:
             try:
                 os.mkdir('DEBIAN')
             except OSError:
                 raise SystemExit(
                     sys.argv[0] + ': Cannot create "DEBIAN" directory.')
-
-        if os.path.isdir('DEBIAN'):
             task2 = subtask_mod.Task(self._tar.get_cmdline() + [
-                os.path.join(os.pardir, 'control.tar.gz')])
+                os.path.join(os.pardir, control_file)
+            ])
             task2.run(directory='DEBIAN', replace=(os.curdir, 'DEBIAN'))
-        else:
-            task2 = subtask_mod.Task(
-                self._tar.get_cmdline() + ['control.tar.gz'])
-            task2.run(replace=(os.curdir, 'DEBIAN'))
-        if task2.get_exitcode():
-            raise SystemExit(
-                sys.argv[0] + ': Error code ' + str(task2.get_exitcode()) +
-                ' received from "' + task2.get_file() + '".'
-            )
-        self._remove('control.tar.gz')
+        self._remove(control_file)
 
     def run(self):
         """
