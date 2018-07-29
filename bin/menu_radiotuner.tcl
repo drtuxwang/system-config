3038de71fea019df5227dc1589bb38aee784a87d
Colin Kong <hob4bit@googlemail.com>
2018-07-29 17:41:06 +0100

    Update radio tuner

--- bin/menu_radiotuner.tcl
+++ bin/menu_radiotuner.tcl
@@ -3,55 +3,45 @@
 . config -bg "#cccccc"
 
 frame .english -bg "#cccccc"
-    button .english.bbcradio -width 10 -bg "#ffcc00" -text "BBC Radios" -command {
-        exec firefox http://www.bbc.co.uk/radio/player/bbc_radio_one &
-    }
-    pack .english.bbcradio -side top
-
     button .english.magic -width 10 -bg "#ffcc00" -text "Magic 105.4" -command {
-        exec vlc --http-reconnect http://tx.whatson.com/icecast.php?i=magic1054.mp3 &
+        exec xweb http://magic1054.radio.net/ &
     }
     pack .english.magic -side top
 pack .english -side top -fill x
 
 frame .chinese -bg "#cccccc"
     button .chinese.988 -width 10 -bg "#ffff00" -text "988最好听" -command {
-        exec vlc --http-reconnect http://starrfm.rastream.com/starrfm-988 &
+        exec xweb http://listen.988.com.my/ &
     }
     pack .chinese.988 -side top
 
-    button .chinese.rthk -width 10 -bg "#ffff00" -text "RTHK Radios" -command {
-        exec firefox http://programme.rthk.hk/channel/radio/player_popup.php?rid=168&player=mp3&type=live &
-    }
-    pack .chinese.rthk -side top
-
     button .chinese.rthk1 -width 10 -bg "#ffff00" -text "RTHK 1" -command {
-        exec vlc --http-reconnect http://stm.rthk.hk/radio1 &
+        exec xweb http://www.rthk.hk/radio/radio1/ &
     }
     pack .chinese.rthk1 -side top
 
     button .chinese.rthk2 -width 10 -bg "#ffff00" -text "RTHK 2" -command {
-        exec vlc --http-reconnect http://stm.rthk.hk/radio2 &
+        exec xweb http://www.rthk.hk/radio/radio2/ &
     }
     pack .chinese.rthk2 -side top
 
     button .chinese.rthk3 -width 10 -bg "#ffff00" -text "RTHK 3" -command {
-        exec vlc --http-reconnect http://stm.rthk.hk/radio3 &
+        exec xweb http://www.rthk.hk/radio/radio3/ &
     }
     pack .chinese.rthk3 -side top
 
     button .chinese.rthk4 -width 10 -bg "#ffff00" -text "RTHK 4" -command {
-        exec vlc --http-reconnect http://stm.rthk.hk/radio4 &
+        exec xweb http://www.rthk.hk/radio/radio4/ &
     }
     pack .chinese.rthk4 -side top
 
     button .chinese.rthk5 -width 10 -bg "#ffff00" -text "RTHK 5" -command {
-        exec vlc --http-reconnect http://stm.rthk.hk/radio5 &
+        exec xweb http://www.rthk.hk/radio/radio5/ &
     }
     pack .chinese.rthk5 -side top
 
     button .chinese.rthkpth -width 10 -bg "#ffff00" -text "RTHK普通話" -command {
-        exec vlc --http-reconnect http://stm.rthk.hk/radiopth &
+        exec xweb http://www.rthk.hk/radio/pth/ &
     }
     pack .chinese.rthkpth -side top
 pack .chinese -side top -fill x
