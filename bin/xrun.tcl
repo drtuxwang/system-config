549232ec5894c9c3b376590b69c4506b305d616f
Dr Colin Kong <hob4bit@googlemail.com>
2018-11-29 09:59:55 +0000

    Add "Copy" button to "xrun.tcl"

--- bin/xrun.tcl
+++ bin/xrun.tcl
@@ -15,8 +15,12 @@ frame .toolbar -bg "#cccccc"
 
     pack .toolbar -side top -fill x
 
-    button .toolbar.new -width 4 -bg "#ff0000" -text "New" -command {set command {}}
-    pack .toolbar.new -side left
+    button .toolbar.cut -width 4 -bg "#ff0000" -text "Cut" -command {set command {}}
+    pack .toolbar.cut -side left
+    button .toolbar.copy -width 4 -bg "#ffcc00" -text "Copy" -command {
+        exec echo "$command" | xclip -in &
+    }
+    pack .toolbar.copy -side left
     button .toolbar.paste -width 4 -bg "#ffcc00" -text "Paste" -command {
         catch {exec xclip -out -selection -c text} command
     }
