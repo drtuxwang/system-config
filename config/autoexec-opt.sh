002fa2b2b9cde1ea8b56e3412de9224b1e3abb2e
Dr Colin Kong <hob4bit@googlemail.com>
2018-12-23 05:42:07 +0000

    Add audio volume control example

--- config/autoexec-opt.sh
+++ config/autoexec-opt.sh
@@ -1,12 +1,21 @@
+# Fix display resolution:
 # set_vga VGA-0 1024 768 60 &
 # set_vga VGA1 1440 900 60 &
 # xreset DVI-I=1360x768
+
+# Fix audio settings:
 # pactl set-card-profile 0 output:hdmi-stereo+input:analog-stereo  # HDMI-1
 # pactl set-card-profile 0 output:hdmi-stereo-extra1+input:analog-stereo  # HDMI-2
 # pactl set-card-profile 0 off
-# setxkbmap us  # "gb", "de", "us"
-# xset m 2,16  # Slow mouse
+# pactl set-sink-volume 0 100%  # pactl list short sinks
 
+# Fix mouse speed:
+# xset m 2,16
+
+# Fix keyboard language:
+# setxkbmap us  # "gb", "de", "us"
+
+# Start applications:
 # start_app -pname=chromium-browser -timeout=60 chromium &
 # start_app -pname=google-chrome -timeout=60 chrome &
 # start_app -timeout=60 firefox &
