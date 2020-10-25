48aa6d0a84f99398c5263dfea56ac080ed06b722
Dr Colin Kong <colin.kong@playtech.com>
2020-10-25 09:51:46 +0000

    Change "ungz.py" & "unxz.py" to output locally

--- bin/ungz.py
+++ bin/ungz.py
@@ -22,11 +22,17 @@ class Options:
         self._args = None
         self.parse(sys.argv)
 
-    def get_gzip(self):
+    def get_archives(self):
+        """
+        Return archive files.
+        """
+        return self._args.archives
+
+    def get_command(self):
         """
         Return gzip Command class object.
         """
-        return self._gzip
+        return self._command
 
     def _parse_args(self, args):
         parser = argparse.ArgumentParser(
@@ -47,8 +53,8 @@ class Options:
         """
         self._parse_args(args[1:])
 
-        self._gzip = command_mod.Command('gzip', errors='stop')
-        self._gzip.set_args(['-d', '-k'] + self._args.archives)
+        self._command = command_mod.Command('gzip', errors='stop')
+        self._command.set_args(['-d', '--stdout'])
 
 
 class Main:
@@ -89,7 +95,17 @@ class Main:
         """
         options = Options()
 
-        subtask_mod.Exec(options.get_gzip().get_cmdline()).run()
+        cmdline = options.get_command().get_cmdline()
+        for file in options.get_archives():
+            if file.endswith('.gz'):
+                print("{0:s}:".format(file))
+                output = os.path.basename(file)[:-3]
+                task = subtask_mod.Batch(cmdline + [file])
+                task.run(file=output)
+                if task.get_exitcode():
+                    for line in task.get_error():
+                        print(line, file=sys.stderr)
+                    raise SystemExit(1)
 
 
 if __name__ == '__main__':
--- bin/unxz.py
+++ bin/unxz.py
@@ -22,6 +22,12 @@ class Options:
         self._args = None
         self.parse(sys.argv)
 
+    def get_archives(self):
+        """
+        Return archive files.
+        """
+        return self._args.archives
+
     def get_command(self):
         """
         Return xz Command class object.
@@ -59,7 +65,7 @@ class Options:
         self._parse_args(args[1:])
 
         self._command = command_mod.Command('xz', errors='stop')
-        self._command.set_args(['-d', '-k'] + self._args.archives)
+        self._command.set_args(['-d', '--stdout'])
         self._set_libraries(self._command)
 
 
@@ -101,7 +107,17 @@ class Main:
         """
         options = Options()
 
-        subtask_mod.Exec(options.get_command().get_cmdline()).run()
+        cmdline = options.get_command().get_cmdline()
+        for file in options.get_archives():
+            if file.endswith('.xz'):
+                print("{0:s}:".format(file))
+                output = os.path.basename(file)[:-3]
+                task = subtask_mod.Batch(cmdline + [file])
+                task.run(file=output)
+                if task.get_exitcode():
+                    for line in task.get_error():
+                        print(line, file=sys.stderr)
+                    raise SystemExit(1)
 
 
 if __name__ == '__main__':
