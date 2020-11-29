b05f8aee9a61ce4b7438c43bf6d3554fcbed9d9f
Dr Colin Kong <colin.kong@playtech.com>
2020-11-29 08:28:20 +0000

    Change Kubernetes images save file name

--- kubernetes/kubernetes-save.sh
+++ kubernetes/kubernetes-save.sh
@@ -6,7 +6,7 @@ ETCD_VERSION=$(docker images | grep "^k8s.gcr.io/etcd " | awk 'NR==1 {print $2}'
 PAUSE_VERSION=$(docker images | grep "^k8s.gcr.io/pause " | awk 'NR==1 {print $2}')
 CALICO_VERSION=$(docker images | grep "^calico/cni " | awk 'NR==1 {print $2}')
 
-FILES="
+IMAGES="
 k8s.gcr.io/kube-apiserver:$KUBE_VERSION
 k8s.gcr.io/kube-controller-manager:$KUBE_VERSION
 k8s.gcr.io/kube-scheduler:$KUBE_VERSION
@@ -15,20 +15,22 @@ k8s.gcr.io/coredns:$COREDNS_VERSION
 k8s.gcr.io/etcd:$ETCD_VERSION
 k8s.gcr.io/pause:$PAUSE_VERSION
 "
-CREATED=$(docker inspect $FILES | sed -e 's/"/ /g' | sort -r | awk '/Created/ {print $3; exit}')
-IMAGES_FILE="kubernetes-control-plane-${KUBE_VERSION}_images.tar"
-echo "docker save $FILES -o $IMAGES_FILE"
-docker save $FILES -o $IMAGES_FILE
-touch -d "$CREATED" $IMAGES_FILE
+FILE="kubernetes_${KUBE_VERSION}_control-pane.tar"
+CREATED=$(docker inspect $IMAGES | sed -e 's/"/ /g' | sort -r | awk '/Created/ {print $3; exit}')
 
-FILES="
+echo "docker save $IMAGES -o $FILE"
+docker save $IMAGES -o $FILE
+touch -d "$CREATED" $FILE
+
+IMAGES="
 calico/cni:$CALICO_VERSION
 calico/kube-controllers:$CALICO_VERSION
 calico/node:$CALICO_VERSION
 calico/pod2daemon-flexvol:$CALICO_VERSION
 "
-CREATED=$(docker inspect $FILES | sed -e 's/"/ /g' | sort -r | awk '/Created/ {print $3; exit}')
-IMAGES_FILE="kubernetes-calico-cni-${CALICO_VERSION}_images.tar"
-echo "docker save $FILES -o $IMAGES_FILE"
-docker save $FILES -o $IMAGES_FILE
-touch -d "$CREATED" $IMAGES_FILE
+FILE="calico_${CALICO_VERSION}_cni.tar"
+CREATED=$(docker inspect $IMAGES | sed -e 's/"/ /g' | sort -r | awk '/Created/ {print $3; exit}')
+
+echo "docker save $IMAGES -o $FILE"
+docker save $IMAGES -o $FILE
+touch -d "$CREATED" $FILE
