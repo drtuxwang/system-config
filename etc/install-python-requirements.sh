3585eae92a13a93142096f96e3b0be745e5087b5
Dr Colin Kong <colin.kong@playtech.com>
2021-02-28 14:03:14 +0000

    Clean up Python 3 pip installation

--- etc/install-python-requirements.sh
+++ etc/install-python-requirements.sh
@@ -5,6 +5,7 @@
 
 # Optional input environment
 PYTHON=${1-}
+FLAG=${2-}
 
 if [ ! -x "$PYTHON" ]
 then
@@ -14,8 +15,8 @@ fi
 
 
 read_requirements() {
-    if [ -f "$1" ]
-    then
+    while [ $# != 0 ]
+    do
         echo "Processing \"$1\"..."
         for PACKAGE in $(sed -e "s/#.*//;s/>=/==/g" $1)
         do
@@ -23,28 +24,54 @@ read_requirements() {
             VERSION=${PACKAGE#*==}
             REQUIREMENTS=$(echo "$REQUIREMENTS" | grep -v "^$NAME=="; echo "$NAME==$VERSION" | grep -v ==None)
         done
+        shift
+    done
+}
+
+
+install_pip() {
+    case $PYTHON_VERSION in
+    2.[67]|3.[345])
+        GETPIP="https://bootstrap.pypa.io/$PYTHON_VERSION/get-pip.py"
+        ;;
+    *)
+        GETPIP="https://bootstrap.pypa.io/get-pip.py"
+        ;;
+    esac
+
+    if [ ! "$($PYTHON -m pip --version 2>&1 | grep "^pip ")" ]
+    then
+        echo "curl --location --progress-bar $GETPIP | $PYTHON"
+        curl --location --progress-bar $GETPIP | $PYTHON || exit 1
+        echo "Installed!"
     fi
+
+    echo "$INSTALL --no-warn-script-location "$(echo "$REQUIREMENTS" | egrep "^(pip|wheel)==")
+    $INSTALL $(echo "$REQUIREMENTS" | egrep "^(pip|wheel)==") 2>&1 | egrep -v "already satisfied:|pip version|--upgrade pip"
+    [[ ${PIPESTATUS[0]} = 0 ]] || exit 1
+    echo "Installed!"
 }
 
 
 install_packages() {
-    umask 022
-    INSTALL="$PYTHON -m pip install"
-    [[ -w "$($PYTHON -help 2>&1 | grep usage: | awk '{print $2}')" ]] && INSTALL="$INSTALL --user"
-    [[ "$(uname)" = Darwin ]] && export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/zlib/lib/pkgconfig"
+    export CRYPTOGRAPHY_DONT_BUILD_RUST=1
 
-    $INSTALL $(echo "$REQUIREMENTS" | egrep "^(pip|wheel)==") 2>&1 | egrep -v "already satisfied:|pip version|--upgrade pip"
+    echo "$INSTALL --no-warn-script-location "$REQUIREMENTS
     $INSTALL --no-warn-script-location $REQUIREMENTS 2>&1 | egrep -v "already satisfied:|pip version|--upgrade pip"
-    [[ ${PIPESTATUS[0]} = 0 ]] && echo "Installed!"
-    return ${PIPESTATUS[0]}
+    [[ ${PIPESTATUS[0]} = 0 ]] || exit 1
+    echo "Installed!"
 }
 
 
-export CRYPTOGRAPHY_DONT_BUILD_RUST=1
+umask 022
+INSTALL="$PYTHON -m pip install"
+[[ -w "$($PYTHON -help 2>&1 | grep usage: | awk '{print $2}')" ]] || INSTALL="$INSTALL --user"
+[[ "$(uname)" = Darwin ]] && export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/zlib/lib/pkgconfig"
 
 REQUIREMENTS=
-read_requirements "${0%/*}/python-requirements.txt"
-VERSION=$($PYTHON --version 2>&1 | grep "^Python [0-9][.][0-9][.]" | awk '{print $2}' | cut -f1-2 -d.)
-read_requirements "${0%/*}/python-requirements_$VERSION.txt"
-install_packages
-exit $?
+PYTHON_VERSION=$($PYTHON --version 2>&1 | awk '/^Python [1-9]/{print $2}' | cut -f1-2 -d.)
+read_requirements "${0%/*}/python-requirements.txt" "${0%/*}/python-requirements_$PYTHON_VERSION.txt"
+
+install_pip
+[[ "$FLAG" != piponly ]] && install_packages
+exit 0
