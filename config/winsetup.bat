357ad924d5f983524176e61f35d5bf08eb159334
Dr Colin Kong <colin.kong@playtech.com>
2021-07-24 13:26:26 +0100

    Fix Busybox 64bit installation

--- config/winsetup.bat
+++ config/winsetup.bat
@@ -1,14 +1,25 @@
 @echo off
 
 ver
+
 for /f %%a in ('dir /b /s ..\software\7zip_*_win*x86.exe') do (
     echo Running "%%a" installer...
     "%%a" -oc:\software -y
 )
-for /f %%a in ('dir /b /s ..\software\busybox_*_win*x86.7z') do (
-    echo Installing "%%a" archive...
-    call c:\software\bin\7z.bat x -oc:\software -y "%%a"
-)
+
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" goto win64
+if "%PROCESSOR_ARCHITEW6432%"=="AMD64" goto win64
+    for /f %%a in ('dir /b /s ..\software\busybox_*_win??x86.7z') do (
+        echo Installing "%%a" archive...
+        call c:\software\bin\7z.bat x -oc:\software -y "%%a"
+    )
+    goto install
+:win64
+    for /f %%a in ('dir /b /s ..\software\busybox_*_win64*x86.7z') do (
+        echo Installing "%%a" archive...
+        call c:\software\bin\7z.bat x -oc:\software -y "%%a"
+    )
+:install
 
 echo.
 echo Running "net use s: \\vboxsvr\shared"...
