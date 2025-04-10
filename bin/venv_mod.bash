87eb7a8013bc650d99103bd944a047f74c813838
Dr Colin Kong <dr.colinkong@googlemail.com>
2025-04-10 13:28:10 +0100

    Fix xce4-terminal copy

--- bin/venv_mod.bash
+++ bin/venv_mod.bash
@@ -84,8 +84,8 @@ exec \"\${0%/*}/python$VERSION\" \"\$@\""
     unset IFS
     [[ $VERSION =~ 2.* ]] && cp -p $PYTHON_DIR/lib/libpython*.so.* $VIRTUAL_ENV.part/lib 2> /dev/null
 
-    [ "$VENV_POSTINST" ] && VIRTUAL_ENV=$VIRTUAL_ENV.part $VENV_POSTINST
-    fmod -R "$("$VIRTUAL_ENV.part/bin/$VENV_PYTHON" -m pip show pip | grep "^Location: " | sed -e "s/Location: //")" > /dev/null 2>&1
+    [ "$VENV_POSTINST" ] && VIRTUAL_ENV="$VIRTUAL_ENV.part" $VENV_POSTINST
+    fmod -R "$VIRTUAL_ENV.part" > /dev/null 2>&1
     mv "$VIRTUAL_ENV.part" "$VIRTUAL_ENV"
 }
 
