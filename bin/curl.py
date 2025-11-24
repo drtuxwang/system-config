68d33c93751f4c3e7e10f8116ff5c7f3ed36ccac
Dr Colin Kong <dr.colinkong@googlemail.com>
2025-11-24 13:15:10 +0000

    Add curl wrapper

---
+++ bin/curl.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+Wrapper for "curl" command
+"""
+
+import os
+import signal
+import sys
+
+from command_mod import Command
+from subtask_mod import Exec
+
+
+class Main:
+    """
+    Main class
+    """
+
+    def __init__(self) -> None:
+        try:
+            self.config()
+            sys.exit(self.run())
+        except (EOFError, KeyboardInterrupt):
+            sys.exit(114)
+        except SystemExit as exception:
+            sys.exit(exception)  # type: ignore
+
+    @staticmethod
+    def config() -> None:
+        """
+        Configure program
+        """
+        if hasattr(signal, 'SIGPIPE'):
+            signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+        if os.linesep != '\n':
+            def _open(file, *args, **kwargs):  # type: ignore
+                if 'newline' not in kwargs and args and 'b' not in args[0]:
+                    kwargs['newline'] = '\n'
+                return open(str(file), *args, **kwargs)
+
+    @staticmethod
+    def run() -> int:
+        """
+        Start program
+        """
+        curl = Command('curl', errors='stop')
+        curl.extend_args(['-H', 'Accept-Language: en'] + sys.argv[1:])
+
+        Exec(curl.get_cmdline()).run()
+
+        return 0
+
+
+if __name__ == '__main__':
+    if '--pydoc' in sys.argv:
+        help(__name__)
+    else:
+        Main()
