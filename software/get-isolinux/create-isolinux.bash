dfb5e6f1b4e2e05467f659b5c161fccac03a7550
Dr Colin Kong <dr.colinkong@googlemail.com>
2026-04-29 09:51:42 +0100

    Clean up isolinux installer

--- software/get-isolinux/create-isolinux.bash
+++ software/get-isolinux/create-isolinux.bash
@@ -5,14 +5,14 @@
 
 set -eu
 
-DIR=$(realpath "${0%/*}/..")
+DIR=$(realpath "${0%/*}")
 VERSION=$(awk '/^menu label \^Debian Live / {print $5; exit}' "$DIR/isolinux/isolinux.cfg")
 VOLUME="DEBIANLIVE"
 FILE="${DIR%/*}/debianlive_${VERSION}_amd64.iso"
 
 echo "Generating ISO boot disk: $FILE"
 
-cd "$DIR"
+cd "$DIR/.."
 genisoimage \
     -iso-level 3 -joliet-long -rational-rock -input-charset utf-8 \
     -appid GENISOIMAGE-$(genisoimage -version | awk 'NR==1 {print $2}') \
