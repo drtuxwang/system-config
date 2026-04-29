6f9c525928650683b737a356109ed791e70cf197
Dr Colin Kong <dr.colinkong@googlemail.com>
2026-04-29 09:29:47 +0100

    Clean u isolinux

--- software/get-isolinux.bash
+++ software/get-isolinux.bash
@@ -21,9 +21,9 @@ app_settings() {
         mv usr/lib/syslinux/vesamenu.c32 isolinux/
         cp ${0%.*}/isolinux.cfg isolinux/
         cp ${0%.*}/README-isolinux.md .
-        cp ${0%.*}/create-boot-iso.bash isolinux/
+        cp ${0%.*}/create-isolinux.bash .
         touch -r isolinux/isolinux.bin \
-            README-isolinux.md isolinux/create-boot-iso.bash isolinux/isolinux.cfg
+            README-isolinux.md create-isolinux.bash isolinux/isolinux.cfg
     "
     APP_REMOVE="
         usr/
--- software/get-isolinux/README-isolinux.md
+++ software/get-isolinux/README-isolinux.md
@@ -3,7 +3,7 @@
 * BIOS boot loader for CD/DVD drives from Debian 7
 * Newer Syslinux versions are unnecessary over complex
 <pre>
-isolinux/create-boot-iso.bash  Generate ISO boot image for BIOS system
+create-isolinux.bash           Generate ISOLINUX boot image for BIOS system
 isolinux/isolinux.bin          SYSLINUX ISO boot loader
 isolinux/isolinux.cfg          SYSLINUX ISO boot menu (edit as required)
 isolinux/vesamenu.c32          SYSLINUX ISO graphical boot menu
