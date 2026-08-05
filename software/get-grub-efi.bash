127fbf5829e1f95583662b1ee71d739188732c1e
Dr Colin Kong <dr.colinkong@googlemail.com>
2026-08-05 16:22:13 +0100

    Update shim-helper

--- software/get-grub-efi.bash
+++ software/get-grub-efi.bash
@@ -17,8 +17,8 @@ app_settings() {
     REPO="https://deb.debian.org/debian/pool"
     APP_FILES="
         $REPO/main/g/grub-efi-amd64-signed/grub-efi-amd64-signed_1+2.12+9+deb13u2_amd64.deb
-        $REPO/main/s/shim-helpers-amd64-signed/shim-helpers-amd64-signed_1+15.8+1_amd64.deb
-        $REPO/main/s/shim-signed/shim-signed_1.50+16.1-2_amd64.deb
+        $REPO/main/s/shim-helpers-amd64-signed/shim-helpers-amd64-signed_1+16.1+2~deb13u1_amd64.deb
+        $REPO/main/s/shim-signed/shim-signed_1.51~1+deb13u1+16.1-2~deb13u1_amd64.deb
         ${0%/*}/files/grub.cfg
         ${0%/*}/files/install-grub-efi.bash
         ${0%/*}/files/README-grub-efi.md
@@ -46,8 +46,8 @@ app_settings_arm() {
     REPO="https://deb.debian.org/debian/pool"
     APP_FILES="
         $REPO/main/g/grub-efi-arm64-signed/grub-efi-arm64-signed_1+2.12+9+deb13u2_arm64.deb
-        $REPO/main/s/shim-helpers-arm64-signed/shim-helpers-arm64-signed_1+15.8+1_arm64.deb
-        $REPO/main/s/shim-signed/shim-signed_1.50+16.1-2_arm64.deb
+        $REPO/main/s/shim-helpers-arm64-signed/shim-helpers-arm64-signed_1+16.1+2~deb12u1_arm64.deb
+        $REPO/main/s/shim-signed/shim-signed_1.51~1+deb12u1+16.1-2~deb12u1_arm64.deb
         ${0%/*}/files/grub.cfg-arm
         ${0%/*}/files/install-grub-efi.bash
         ${0%/*}/files/README-grub-efi.md-arm
