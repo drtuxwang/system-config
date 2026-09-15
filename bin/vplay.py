feeb7277bddd3b21acea8a3d8658b5a3fb646b72
Dr Colin Kong <dr.colinkong@googlemail.com>
2026-09-15 10:53:40 +0100

    Move eron config_mod extensions to mimetypes

--- bin/vplay.py
+++ bin/vplay.py
@@ -11,8 +11,9 @@ import sys
 from pathlib import Path
 from typing import List
 
+import magic  # type: ignore
+
 from command_mod import Command
-from config_mod import Config
 from subtask_mod import Task
 
 
@@ -106,9 +107,11 @@ class Main:
 
     @staticmethod
     def _get_files(directory: str) -> List[str]:
-        files: list = []
-        for pattern in Config().get('video_extensions'):
-            files.extend(Path(directory).glob(f'*{pattern}'))
+        files = [
+            x
+            for x in Path(directory).iterdir()
+            if magic.from_file(x, mime=True).startswith('video/')
+        ]
         return sorted([str(x) for x in files])
 
     def run(self) -> int:
